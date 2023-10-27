e5e853bd54da3bb0e81d3a0a75f9201cea3d4188
awuyadaniel <awuyadanielz@gmail.com>
2023-10-27 15:57:08 +0100

    first commit

--- Food/settings.py
+++ Food/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-o)je8z!l5i9563twh@$^m43$ys36yvz_uie^&$do13rj6_q1($
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
